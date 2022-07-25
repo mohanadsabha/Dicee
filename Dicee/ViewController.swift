f31f4080026a97e4b49ff9d5721e52b12b1836dc
Mohanad Abu Sabha <mohannad.naxol@gmail.com>
2022-07-25 04:45:08 +0300

    writing code

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var diceImageOne: UIImageView!
+    @IBOutlet weak var diceImageTwo: UIImageView!
+    // array of image literals
+    let dices = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func rollButton(_ sender: UIButton) {
+        // pick random image from the array and assign it to the image view
+        diceImageOne.image = dices.randomElement()
+        diceImageTwo.image = dices.randomElement()
     }
-
-
 }
-
